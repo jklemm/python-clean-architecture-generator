13c3fec0c1921c2c92e5f44054ef98de101ac84f
Jorge <jorge.klemm@hotmail.com>
2015-12-09 13:50:51 -0200

    Added more endpoints to generate code

--- cag.py
+++ cag.py
@@ -20,21 +20,48 @@ def generate_file(content, filename):
     newfile.close()
 
 
-def generate_entity(model_name):
-    snakecase_model_name = model_name.lower()
-    camelcase_model_name = model_name.capitalize()
-    content = j2_env.get_template('entity.jinja2').render(entity=camelcase_model_name)
-    filename = OUTPUT_DIR + '{0}_entity.py'.format(snakecase_model_name)
+def generate_entity():
+    content = j2_env.get_template('entity.jinja2').render(snake=snake_name, camel=camel_name)
+    filename = OUTPUT_DIR + 'core/entities/{0}_entity.py'.format(snake_name)
     generate_file(content, filename)
 
 
-def generate_gateway(model_name):
-    snakecase_model_name = model_name.lower()
-    camelcase_model_name = model_name.capitalize()
-    content = j2_env.get_template('gateway.jinja2').render(gateway=camelcase_model_name)
-    filename = OUTPUT_DIR + '{0}_gateway.py'.format(snakecase_model_name)
+def generate_struct():
+    content = j2_env.get_template('struct.jinja2').render(snake=snake_name, camel=camel_name)
+    filename = OUTPUT_DIR + 'core/structs/{0}_struct.py'.format(snake_name)
     generate_file(content, filename)
 
-entity_name = 'rentabilidade'
-generate_entity(entity_name)
 
+def generate_usecase():
+    content = j2_env.get_template('usecase.jinja2').render(snake=snake_name, camel=camel_name)
+    filename = OUTPUT_DIR + 'core/usecases/{0}_usecase.py'.format(snake_name)
+    generate_file(content, filename)
+
+
+def generate_gateway():
+    content = j2_env.get_template('gateway.jinja2').render(snake=snake_name, camel=camel_name)
+    filename = OUTPUT_DIR + 'gateways/{0}_gateway.py'.format(snake_name)
+    generate_file(content, filename)
+
+
+def generate_gateway_factory():
+    content = j2_env.get_template('gateway_factory.jinja2').render(snake=snake_name, camel=camel_name)
+    filename = OUTPUT_DIR + 'factories/{0}_gateway_factory.py'.format(snake_name)
+    generate_file(content, filename)
+
+
+def generate_usecase_factory():
+    content = j2_env.get_template('usecase_factory.jinja2').render(snake=snake_name, camel=camel_name)
+    filename = OUTPUT_DIR + 'factories/{0}_usecase_factory.py'.format(snake_name)
+    generate_file(content, filename)
+
+name = 'rentabilidade'
+snake_name = name.lower()
+camel_name = name.capitalize()
+
+generate_entity()
+generate_struct()
+generate_gateway()
+generate_usecase()
+generate_gateway_factory()
+generate_usecase_factory()
