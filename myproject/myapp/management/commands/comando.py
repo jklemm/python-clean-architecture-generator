b5ac181489749c37d38f991b33c0192b7b610eae
Jorge <jorge.klemm@hotmail.com>
2016-01-19 18:05:07 -0200

    Added Django 1.3.7 framework to requirements

---
+++ myproject/myapp/management/commands/comando.py
@@ -0,0 +1,9 @@
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    args = '<poll_id poll_id ...>'
+    help = 'Closes the specified poll for voting'
+
+    def handle(self, *args, **options):
+        self.stdout.write('Chamada efetuada!\n')
